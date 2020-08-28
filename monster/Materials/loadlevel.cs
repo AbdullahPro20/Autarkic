3439c8db70c806eb2faef28ac83191f1fd363eaa
AbdullahPro20 <70176402+AbdullahPro20@users.noreply.github.com>
2020-08-28 08:46:16 +0500

    Add files via upload

---
+++ monster/Materials/loadlevel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadlevel : MonoBehaviour
+{
+    public string levelname;
+    public bool loadonstart;
+    public bool wait;
+    public float NUM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (wait) {
+
+            StartCoroutine(ExampleCoroutine());
+
+        }
+
+        if (loadonstart)
+        {
+            SceneManager.LoadScene(levelname);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void loadlevels() {
+
+       // SceneManager.LoadScene(levelname);
+       // move.score = 0;
+        StartCoroutine(ExampleCoroutine1());
+    }
+    public void exitlevel() {
+
+        Application.Quit();
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+   
+       
+        yield return new WaitForSeconds(NUM);
+ 
+        SceneManager.LoadScene(levelname);
+    }
+    IEnumerator ExampleCoroutine1() 
+    {
+        yield return new WaitForSeconds(1);
+       
+        SceneManager.LoadScene(levelname);
+       
+    }
+}
---
+++ monster/chase.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class chase : MonoBehaviour
+{
+    //public GameObject destoryEnemy;
+    public static bool dead;
+    public GameObject player;
+    public Animator anim;
+    public static float health=1;
+    public bool fallback;
+    public GameObject playerhealth;
+    int a;
+    bool healthdec;
+    // Use this for initialization
+
+    IEnumerator Example()
+    {
+        Debug.Log("its working hahaha");
+
+        yield return new WaitForSeconds(6f);
+        anim.SetBool("block", false);
+       
+        Debug.Log("its totally working hahaha");
+        
+       
+
+    }
+
+
+        void Start()
+    {
+     
+        dead = false;
+    }
+
+   
+     IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        player.GetComponent<Animator>().SetBool("fallback", true);
+        playerhealth.GetComponent<Healthbar>().TakeDamage(1);
+        fallback = true;
+        healthdec = true;
+        yield return new WaitForSeconds(2);
+        player.GetComponent<Animator>().SetBool("fallback", false);
+        //After we have waited 5 seconds print the time again.
+        fallback = false;
+    }
+    void FixedUpdate()
+    {
+        if (healthdec) {
+            playerhealth.GetComponent<Healthbar>().TakeDamage(0.5f);
+            healthdec = false;
+        }
+        if (fallback) {
+         
+            player.transform.position = Vector3.MoveTowards(player.transform.position, player.GetComponent<charactermove>().fallpoint.transform.position, 5 * Time.deltaTime);
+           
+        }
+        if (dead == false) {
+
+        if (Vector3.Distance(player.transform.position, this.transform.position) < 15)
+        {
+            Vector3 direction = player.transform.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("idle", false);
+                if (direction.magnitude >2f)
+                {
+
+                    anim.SetBool("walk", true);
+                    anim.SetBool("attack", false);
+                    anim.SetBool("idle", false);
+                    this.transform.Translate(0, 0, 0.04f);
+
+                  
+                }
+                else
+                {
+
+                anim.SetBool("walk", false);
+                    anim.SetBool("idle", false);
+                    anim.SetBool("attack", true);
+                 
+                    StartCoroutine(ExampleCoroutine());
+
+                }
+        }
+        
+            
+            else
+        {
+            anim.SetBool("idle", true);
+            anim.SetBool("walk", false);
+            anim.SetBool("attack", false);
+                 
+        }
+        }
+    }
+}
